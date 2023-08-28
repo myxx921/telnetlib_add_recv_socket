15219799811f010538811e331df4de102f1b7306
myxx921 <124624219+myxx921@users.noreply.github.com>
2023-08-28 17:31:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from distutils.core import setup
 
+setup(
+
+name = 'telnetlib_external_socket',
+version = '1.0',
+py_modules = ['telnet_lib_external_socket'],
+author = 'myxx921',
+author_email = 'myxx921@gmail.com',
+url = 'https://github.com/myxx921/telnetlib_external_socket',
+description = 'added external socket to standart telnetlib',
+)
